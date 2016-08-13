69ea324193d4a54c23c283be6fa1b4325528ab24
Erik Dubois <erik.dubois@gmail.com>
2016-08-13 07:02:47 +0200

    Commit comment : canon driver on Sat Aug 13 07:02:47 CEST 2016

---
+++ install-canon-mg7750-DOES-NOT-WORK.sh
@@ -0,0 +1,67 @@
+#!/bin/bash 
+############################################################################
+#
+#                                       
+#                          ./+o+-       
+#                  yyyyy- -yyyyyy+      
+#               ://+//////-yyyyyyo      
+#           .++ .:/++++++/-.+sss/`      
+#         .:++o:  /++++++++/:--:/-      
+#        o:+o+:++.`..```.-/oo+++++/     
+#       .:+o:+o/.          `+sssoo+/    
+#  .++/+:+oo+o:`             /sssooo.   
+# /+++//+:`oo+o               /::--:.   
+# \+/+o+++`o++o               ++////.   
+#  .++.o+++oo+:`             /dddhhh.   
+#       .+.o+oo:.          `oddhhhh+    
+#        \+.++o+o``-````.:ohdhhhhh+     
+#         `:o+++ `ohhhhhhhhyo++os:      
+#           .o:`.syhhhhhhh/.oo++o`      
+#               /osyyyyyyo++ooo+++/     
+#                   ````` +oo+++o\:    
+#                          `oo++.      
+#                                
+#
+#
+############################################################################
+# Author 	: 	Erik Dubois
+# Website 	: 	http://www.erikdubois.be
+############################################################################
+# More from Erik Dubois
+#
+# Aurora conky and all its components are installed via an other script
+# at http://sourceforge.net/projects/auroraconkytheme/
+# Explanation on the use of this theme can be found at 
+# http://erikdubois.be/
+#
+# Sardi icons can be found at
+# http://sourceforge.net/projects/sardi/
+#
+# Yltra flat icons can be found at 
+# https://github.com/erikdubois/yltra-flat-icon-theme
+############################################################################
+#
+# the option -y has been added. It will autoinstall all. Omit if you do not want that.
+#
+############################################################################
+
+
+############################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. AT YOUR OWN RISK.
+#
+############################################################################
+
+
+
+# http://tutorialforlinux.com/2016/02/09/how-to-install-canon-pixma-mg7750mg7751mg7752mg7753-printers-drivers-on-ubuntu-linux-and-quickstart-scanning-easy-guide/
+sudo apt-get install libxml2 libglade2-0 libpng3 libtiff5 -y
+cd cnijfilter2-5.20-1-deb
+./install.sh
+
+
+############################################################################
+
+#ending
+
+
