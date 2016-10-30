cbba9b4421500c4fb7738a87c901f528fc07df66
Erik Dubois <erik.dubois@gmail.com>
2016-10-30 16:57:19 +0100

    Commit comment :  on Sun Oct 30 16:57:19 CET 2016

---
+++ install-i3-on-solus-v1.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+############################################################################
+# Written to be used on 64 bits computers
+# Author 	: 	Erik Dubois
+# Website 	: 	http://www.erikdubois.be
+############################################################################
+#
+#
+#                  e         e
+#                eee       ee
+#               eeee     eee
+#           wwwwwwwwweeeeee
+#        wwwwwwwwwwwwwwweee
+#      wwwwwwwwwwwwwwwwwwweeeeeeee
+#     wwwww     eeeeewwwwwweeee
+#    www          eeeewwwwwwe
+#    ww             eewwwwww
+#    w                 wwwww
+#                      wwwww
+#                     wwwww
+#                    wwwww
+#                   wwww
+#                  wwww
+#                wwww
+#              www
+#             ww
+#
+#
+##################################################################################################################
+
+
+sudo eopkg it -y dunst i3 i3blocks i3lock i3status dmenu lxappearance
