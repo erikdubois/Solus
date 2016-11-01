07a12fc79493afa6ee4d5b9a44e86e586fcc0dd2
Erik Dubois <erik.dubois@gmail.com>
2016-11-01 13:28:53 +0100

    Commit comment :  on Tue Nov  1 13:28:53 CET 2016

--- install-third-party-software-v2.sh
+++ install-third-party-software-v2.sh
@@ -123,6 +123,12 @@ sudo eopkg it -y skype*.eopkg;sudo rm *.eopkg
 sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/network/im/slack-desktop/pspec.xml
 sudo eopkg it slack-desktop*.eopkg;sudo rm slack-desktop*.eopkg
 
+echo "fixing hardcoded icon"
+old="Icon=\/usr\/share\/pixmaps\/slack.png"
+new="Icon=slack"
+location="/usr/share/applications/slack.desktop"
+sudo sed -i s/$old/$new/g $location
+
 # Teamspeak
 sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/network/im/teamspeak/pspec.xml
 sudo eopkg it teamspeak3*.eopkg;sudo rm teamspeak3*.eopkg
@@ -146,6 +152,13 @@ sudo eopkg it ocenaudio*.eopkg;sudo rm ocenaudio*.eopkg
 # Pixeluvo
 sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/multimedia/graphics/pixeluvo/pspec.xml
 sudo eopkg it pixeluvo*.eopkg;sudo rm pixeluvo*.eopkg
+
+echo "fixing hardcoded icon"
+old="Icon=\/opt\/pixeluvo\/pixeluvo.png"
+new="Icon=pixeluvo"
+location="/usr/share/applications/pixeluvo.desktop"
+sudo sed -i s/$old/$new/g $location
+
 
 # Plex Media Server
 sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/multimedia/video/plexmediaserver/pspec.xml
@@ -178,6 +191,12 @@ sudo eopkg it spideroak*.eopkg;sudo rm spideroak*.eopkg
 sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/network/download/synology-cloud-station-drive/pspec.xml
 sudo eopkg it synology-cloud-station-drive*.eopkg;sudo rm synology-cloud-station-drive*.eopkg
 
+echo "fixing hardcoded icon"
+old="Icon=\/opt\/Synology\/CloudStation\/images\/ico_72_cloud_station.png"
+new="Icon=ico_72_cloud_station"
+location="/usr/share/applications/synology-cloud-station-drive.desktop"
+sudo sed -i s/$old/$new/g $location
+
 # Teamviewer
 sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/network/util/teamviewer/pspec.xml
 sudo eopkg it -y teamviewer*.eopkg;sudo rm teamviewer*.eopkg
@@ -199,6 +218,7 @@ sudo eopkg it moneydance*.eopkg;sudo rm moneydance*.eopkg
 sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/office/scrivener/pspec.xml
 sudo eopkg it scrivener*.eopkg;sudo rm scrivener*.eopkg
 
+
 # WPS office
 
 sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/office/wps-office/pspec.xml
@@ -208,17 +228,35 @@ sudo eopkg it -y wps-office*.eopkg;sudo rm wps-office*.eopkg
 # PROGRAMMING
 ###############################################################################################
 
-# Android Station
+# Android Studio
 sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/programming/android-studio/pspec.xml
 sudo eopkg it android-studio*.eopkg;sudo rm android-studio*.eopkg
+
+echo "fixing hardcoded icon"
+old="Icon=\/opt\/android-studio\/bin\/studio.png"
+new="Icon=android-studio"
+location="/usr/share/applications/android-studio.desktop"
+sudo sed -i s/$old/$new/g $location
 
 # CLion
 sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/programming/clion/pspec.xml
 sudo eopkg it clion*.eopkg;sudo rm clion*.eopkg
 
+echo "fixing hardcoded icon"
+old="Icon=\/opt\/clion\/bin\/clion.svg"
+new="Icon=clion"
+location="/usr/share/applications/clion.desktop"
+sudo sed -i s/$old/$new/g $location
+
 # Datagrip
 sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/programming/datagrip/pspec.xml
 sudo eopkg it datagrip*.eopkg;sudo rm datagrip*.eopkg
+
+echo "fixing hardcoded icon"
+old="Icon=\/opt\/datagrip\/bin\/product.png"
+new="Icon=datagrip"
+location="/usr/share/applications/datagrip.desktop"
+sudo sed -i s/$old/$new/g $location
 
 # Git Kraken
 sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/programming/gitkraken/pspec.xml
@@ -228,17 +266,42 @@ sudo eopkg it -y gitkraken*.eopkg;sudo rm gitkraken*.eopkg
 sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/programming/idea/pspec.xml
 sudo eopkg it -y idea*.eopkg;sudo rm idea*.eopkg
 
+echo "fixing hardcoded icon"
+old="Icon=\/opt\/idea\/bin\/idea.png"
+new="Icon=idea"
+location="/usr/share/applications/idea.desktop"
+sudo sed -i s/$old/$new/g $location
+
 # PHPStorm
 sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/programming/phpstorm/pspec.xml
 sudo eopkg it phpstorm*.eopkg;sudo rm phpstorm*.eopkg
+
+echo "fixing hardcoded icon"
+old="Icon=\/opt\/phpstorm\/bin\/webide.png"
+new="Icon=phpstorm"
+location="/usr/share/applications/phpstorm.desktop"
+sudo sed -i s/$old/$new/g $location
 
 # Pycharm
 sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/programming/pycharm/pspec.xml
 sudo eopkg it -y pycharm*.eopkg;sudo rm pycharm*.eopkg
 
+echo "fixing hardcoded icon"
+old="Icon=\/opt\/pycharm\/bin\/pycharm.png"
+new="Icon=pycharm"
+location="/usr/share/applications/pycharm.desktop"
+sudo sed -i s/$old/$new/g $location
+
+
 # Rubymine
 sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/programming/rubymine/pspec.xml
 sudo eopkg it rubymine*.eopkg;sudo rm rubymine*.eopkg
+
+echo "fixing hardcoded icon"
+old="Icon=\/opt\/rubymine\/bin\/RMlogo.svg"
+new="Icon=rubymine"
+location="/usr/share/applications/rubymine.desktop"
+sudo sed -i s/$old/$new/g $location
 
 # Sublime Text 3
 sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/programming/sublime-text-3/pspec.xml
@@ -247,6 +310,12 @@ sudo eopkg it -y sublime*.eopkg;sudo rm sublime*.eopkg
 # WebStorm
 sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/programming/webstorm/pspec.xml
 sudo eopkg it webstorm*.eopkg;sudo rm webstorm*.eopkg
+
+echo "fixing hardcoded icon"
+old="Icon=\/opt\/webstorm\/bin\/webstorm.svg"
+new="Icon=webstorm"
+location="/usr/share/applications/webstorm.desktop"
+sudo sed -i s/$old/$new/g $location
 
 ###############################################################################################
 # SECURITY
