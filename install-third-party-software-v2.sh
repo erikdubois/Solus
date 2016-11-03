d99c57440507b7b8b1b379c27dd663c4577fea43
Erik Dubois <erik.dubois@gmail.com>
2016-11-03 12:54:55 +0100

    Commit comment :  on Thu  3 Nov 12:54:55 CET 2016

--- install-third-party-software-v2.sh
+++ install-third-party-software-v2.sh
@@ -56,11 +56,11 @@ sudo eopkg it -y google-chrome-*.eopkg;sudo rm google-chrome-*.eopkg
 
 # Google Chrome Beta
 sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/network/web/browser/google-chrome-beta/pspec.xml
-sudo eopkg it google-chrome-*.eopkg;sudo rm google-chrome-*.eopkg
+sudo eopkg it -y google-chrome-*.eopkg;sudo rm google-chrome-*.eopkg
 
 # Google Chrome dev/unstable
 sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/network/web/browser/google-chrome-unstable/pspec.xml
-sudo eopkg it google-chrome-*.eopkg;sudo rm google-chrome-*.eopkg
+sudo eopkg it -y google-chrome-*.eopkg;sudo rm google-chrome-*.eopkg
 
 # Opera
 sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/network/web/browser/opera-stable/pspec.xml
@@ -72,7 +72,7 @@ sudo eopkg it -y vivaldi*.eopkg;sudo rm *.eopkg
 
 # Vivaldi snapshot
 sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/network/web/browser/vivaldi-snapshot/pspec.xml
-sudo eopkg it vivaldi*.eopkg;sudo rm *.eopkg
+sudo eopkg it -y vivaldi*.eopkg;sudo rm *.eopkg
 
 
 
@@ -90,7 +90,7 @@ sudo eopkg it -y franz*.eopkg;sudo rm franz*.eopkg
 
 # Google Talk Browser plugin
 sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/network/im/google-talkplugin/pspec.xml
-sudo eopkg it google-talkplugin*.eopkg;sudo rm google-talkplugin*.eopkg
+sudo eopkg it -y google-talkplugin*.eopkg;sudo rm google-talkplugin*.eopkg
 
 # Skype for linux alpha
 sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/network/im/skype/pspec.xml
@@ -98,7 +98,7 @@ sudo eopkg it -y skype*.eopkg;sudo rm *.eopkg
 
 # Slack
 sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/network/im/slack-desktop/pspec.xml
-sudo eopkg it slack-desktop*.eopkg;sudo rm slack-desktop*.eopkg
+sudo eopkg it -y slack-desktop*.eopkg;sudo rm slack-desktop*.eopkg
 
 echo "fixing hardcoded icon"
 old="Icon=\/usr\/share\/pixmaps\/slack.png"
@@ -108,11 +108,11 @@ sudo sed -i s/$old/$new/g $location
 
 # Teamspeak
 sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/network/im/teamspeak/pspec.xml
-sudo eopkg it teamspeak3*.eopkg;sudo rm teamspeak3*.eopkg
+sudo eopkg it -y teamspeak3*.eopkg;sudo rm teamspeak3*.eopkg
 
 # Viber
 sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/network/im/viber/pspec.xml
-sudo eopkg it viber*.eopkg;sudo rm *.eopkg
+sudo eopkg it -y viber*.eopkg;sudo rm *.eopkg
 
 echo "fixing hardcoded icon"
 old="Icon=\/usr\/share\/pixmaps\/viber.svg"
@@ -126,7 +126,7 @@ sudo sed -i s/$old/$new/g $location
 
 # Bitwig Studio
 sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/multimedia/music/bitwig-studio/pspec.xml
-sudo eopkg it bitwig-studio*.eopkg;sudo rm bitwig-studio*.eopkg
+sudo eopkg it -y bitwig-studio*.eopkg;sudo rm bitwig-studio*.eopkg
 
 echo "fixing hardcoded icon"
 old="Icon=\/usr\/share\/icons\/hicolor\/scalable\/apps\/bitwig-studio.svg"
@@ -136,11 +136,11 @@ sudo sed -i s/$old/$new/g $location
 
 # OcenAudio
 sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/multimedia/music/ocenaudio/pspec.xml
-sudo eopkg it ocenaudio*.eopkg;sudo rm ocenaudio*.eopkg
+sudo eopkg it -y ocenaudio*.eopkg;sudo rm ocenaudio*.eopkg
 
 # Pixeluvo
 sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/multimedia/graphics/pixeluvo/pspec.xml
-sudo eopkg it pixeluvo*.eopkg;sudo rm pixeluvo*.eopkg
+sudo eopkg it -y pixeluvo*.eopkg;sudo rm pixeluvo*.eopkg
 
 echo "fixing hardcoded icon"
 old="Icon=\/opt\/pixeluvo\/pixeluvo.png"
@@ -151,14 +151,14 @@ sudo sed -i s/$old/$new/g $location
 
 # Plex Media Server
 sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/multimedia/video/plexmediaserver/pspec.xml
-sudo eopkg it plexmediaserver-*.eopkg;sudo rm plexmediaserver-*.eopkg
+sudo eopkg it -y plexmediaserver-*.eopkg;sudo rm plexmediaserver-*.eopkg
 sudo systemd-tmpfiles --create
 sudo systemctl enable plexmediaserver.service
 sudo systemctl start plexmediaserver.service
 
 # Sunvox
 sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/multimedia/music/sunvox/pspec.xml
-sudo eopkg it sunvox*.eopkg;sudo rm sunvox*.eopkg
+sudo eopkg it -y sunvox*.eopkg;sudo rm sunvox*.eopkg
 
 # Spotify
 sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/multimedia/music/spotify/pspec.xml
@@ -170,15 +170,15 @@ sudo eopkg it -y spotify*.eopkg;sudo rm spotify*.eopkg
 
 # PCloud
 sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/network/download/pcloud/pspec.xml
-sudo eopkg it pcloud*.eopkg;sudo rm pcloud*.eopkg
+sudo eopkg it -y pcloud*.eopkg;sudo rm pcloud*.eopkg
 
 # Spideroak
 sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/network/download/spideroak/pspec.xml
-sudo eopkg it spideroak*.eopkg;sudo rm spideroak*.eopkg
+sudo eopkg it -y spideroak*.eopkg;sudo rm spideroak*.eopkg
 
 # Synology Cloud Station Drive
 sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/network/download/synology-cloud-station-drive/pspec.xml
-sudo eopkg it synology-cloud-station-drive*.eopkg;sudo rm synology-cloud-station-drive*.eopkg
+sudo eopkg it -y synology-cloud-station-drive*.eopkg;sudo rm synology-cloud-station-drive*.eopkg
 
 echo "fixing hardcoded icon"
 old="Icon=\/opt\/Synology\/CloudStation\/images\/ico_72_cloud_station.png"
@@ -203,15 +203,15 @@ sudo sed -i s/$old/$new/g $location
 
 # Mendeley Desktop
 sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/office/mendeleydesktop/pspec.xml
-sudo eopkg it mendeleydesktop*.eopkg;sudo rm mendeleydesktop*.eopkg
+sudo eopkg it -y mendeleydesktop*.eopkg;sudo rm mendeleydesktop*.eopkg
 
 # Moneydance
 sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/office/moneydance/pspec.xml
-sudo eopkg it moneydance*.eopkg;sudo rm moneydance*.eopkg
+sudo eopkg it -y moneydance*.eopkg;sudo rm moneydance*.eopkg
 
 # Scrivener
 sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/office/scrivener/pspec.xml
-sudo eopkg it scrivener*.eopkg;sudo rm scrivener*.eopkg
+sudo eopkg it -y scrivener*.eopkg;sudo rm scrivener*.eopkg
 
 
 # WPS office
@@ -225,7 +225,7 @@ sudo eopkg it -y wps-office*.eopkg;sudo rm wps-office*.eopkg
 
 # Android Studio
 sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/programming/android-studio/pspec.xml
-sudo eopkg it android-studio*.eopkg;sudo rm android-studio*.eopkg
+sudo eopkg it -y android-studio*.eopkg;sudo rm android-studio*.eopkg
 
 echo "fixing hardcoded icon"
 old="Icon=\/opt\/android-studio\/bin\/studio.png"
@@ -235,7 +235,7 @@ sudo sed -i s/$old/$new/g $location
 
 # CLion
 sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/programming/clion/pspec.xml
-sudo eopkg it clion*.eopkg;sudo rm clion*.eopkg
+sudo eopkg it -y clion*.eopkg;sudo rm clion*.eopkg
 
 echo "fixing hardcoded icon"
 old="Icon=\/opt\/clion\/bin\/clion.svg"
@@ -245,7 +245,7 @@ sudo sed -i s/$old/$new/g $location
 
 # Datagrip
 sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/programming/datagrip/pspec.xml
-sudo eopkg it datagrip*.eopkg;sudo rm datagrip*.eopkg
+sudo eopkg it -y datagrip*.eopkg;sudo rm datagrip*.eopkg
 
 echo "fixing hardcoded icon"
 old="Icon=\/opt\/datagrip\/bin\/product.png"
@@ -269,7 +269,7 @@ sudo sed -i s/$old/$new/g $location
 
 # PHPStorm
 sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/programming/phpstorm/pspec.xml
-sudo eopkg it phpstorm*.eopkg;sudo rm phpstorm*.eopkg
+sudo eopkg it -y phpstorm*.eopkg;sudo rm phpstorm*.eopkg
 
 echo "fixing hardcoded icon"
 old="Icon=\/opt\/phpstorm\/bin\/webide.png"
@@ -290,7 +290,7 @@ sudo sed -i s/$old/$new/g $location
 
 # Rubymine
 sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/programming/rubymine/pspec.xml
-sudo eopkg it rubymine*.eopkg;sudo rm rubymine*.eopkg
+sudo eopkg it -y rubymine*.eopkg;sudo rm rubymine*.eopkg
 
 echo "fixing hardcoded icon"
 old="Icon=\/opt\/rubymine\/bin\/RMlogo.svg"
@@ -304,7 +304,7 @@ sudo eopkg it -y sublime*.eopkg;sudo rm sublime*.eopkg
 
 # WebStorm
 sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/programming/webstorm/pspec.xml
-sudo eopkg it webstorm*.eopkg;sudo rm webstorm*.eopkg
+sudo eopkg it -y webstorm*.eopkg;sudo rm webstorm*.eopkg
 
 echo "fixing hardcoded icon"
 old="Icon=\/opt\/webstorm\/bin\/webstorm.svg"
@@ -318,7 +318,7 @@ sudo sed -i s/$old/$new/g $location
 
 # Enpass
 sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/security/enpass/pspec.xml
-sudo eopkg it enpass*.eopkg;sudo rm enpass*.eopkg
+sudo eopkg it -y enpass*.eopkg;sudo rm enpass*.eopkg
 
 ###############################################################################################
 # OTHER
@@ -326,4 +326,4 @@ sudo eopkg it enpass*.eopkg;sudo rm enpass*.eopkg
 
 # Google Earth
 sudo eopkg bi --ignore-safety https://raw.githubusercontent.com/solus-project/3rd-party/master/network/web/google-earth/pspec.xml
-sudo eopkg it google-earth*.eopkg;sudo rm google-earth*.eopkg
+sudo eopkg it -y google-earth*.eopkg;sudo rm google-earth*.eopkg
