ee7e7af0f1ff9fd33147a11cf2958a48685c4b05
Erik Dubois <erik.dubois@gmail.com>
2016-11-06 08:07:58 +0100

    Commit comment :  on Sun  6 Nov 08:07:58 CET 2016

--- install-mate-on-solus-v1.sh
+++ install-mate-on-solus-v1.sh
@@ -4,33 +4,7 @@
 # Author 	: 	Erik Dubois
 # Website 	: 	http://www.erikdubois.be
 ############################################################################
-#
-#
-#                  e         e
-#                eee       ee
-#               eeee     eee
-#           wwwwwwwwweeeeee
-#        wwwwwwwwwwwwwwweee
-#      wwwwwwwwwwwwwwwwwwweeeeeeee
-#     wwwww     eeeeewwwwwweeee
-#    www          eeeewwwwwwe
-#    ww             eewwwwww
-#    w                 wwwww
-#                      wwwww
-#                     wwwww
-#                    wwwww
-#                   wwww
-#                  wwww
-#                wwww
-#              www
-#             ww
-#
-#
-##################################################################################################################
 
-# copy/paste the folder i3 in the hidden folder .config
-# logoff and logon as i3
-# top right there is an icon where you can choose your desktop
 
-# this will install 43 packages on nov/2016
-sudo pisi it -c desktop.mate
+# this will install 45 packages on nov/2016
+sudo eopkg it -c desktop.mate
--- install-software-from-Solus-Unstable.sh
+++ install-software-from-Solus-Unstable.sh
@@ -5,51 +5,6 @@
 # Author 	: 	Erik Dubois
 # Website 	: 	http://www.erikdubois.be
 ############################################################################
-#
-#                                       
-#                  e         e      
-#                eee       ee       
-#               eeee     eee        
-#           wwwwwwwwweeeeee         
-#        wwwwwwwwwwwwwwweee         
-#      wwwwwwwwwwwwwwwwwwweeeeeeee  
-#     wwwww     eeeeewwwwwweeee     
-#    www          eeeewwwwwwe       
-#    ww             eewwwwww        
-#    w                 wwwww        
-#                      wwwww       
-#                     wwwww      
-#                    wwwww         
-#                   wwww           
-#                  wwww            
-#                wwww              
-#              www                 
-#             ww                                            
-#
-#
-############################################################################
-# Author 	: 	Erik Dubois
-# Website 	: 	http://www.erikdubois.be
-############################################################################
-# More from Erik Dubois
-#
-# Aurora conky and all its components are installed via an other script
-# at http://sourceforge.net/projects/auroraconkytheme/
-# Explanation on the use of this theme can be found at 
-# http://erikdubois.be/
-#
-# Sardi icons can be found at
-# http://sourceforge.net/projects/sardi/
-#
-# Yltra flat icons can be found at 
-# https://github.com/erikdubois/yltra-flat-icon-theme
-############################################################################
-#
-# the option -y has been added. It will autoinstall all. Omit if you do not want that.
-#
-############################################################################
-
-
 ############################################################################
 #
 #   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. AT YOUR OWN RISK.
@@ -68,7 +23,7 @@ sudo eopkg enable-repo SolusUnstable
 # include them on this line
 # as this changes all the time I have hashtagged it to become comment
 
-
+sudo eopkg upgrade
 #sudo eopkg upgrade peek vccode vlc vscode
 
 
