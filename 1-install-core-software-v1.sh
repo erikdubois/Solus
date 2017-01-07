3dcbcaa94fab77bfce810fd4ccbab0d470e68d5d
Erik Dubois <erik.dubois@gmail.com>
2017-01-07 20:44:00 +0100

    Commit comment :  on Sat Jan  7 20:44:00 CET 2017

--- 1-install-core-software-v1.sh
+++ 1-install-core-software-v1.sh
@@ -16,7 +16,7 @@
 sudo eopkg install -y atom brackets bleachbit cantarell-fonts catfish clipit cmatrix evince evolution 
 sudo eopkg install -y flash-player-nonfree font-manager font-awesome-ttf font-ubuntu-ttf
 sudo eopkg install -y focuswriter geary galculator gcolor3 gconf gimp glances gnome-disk-utility gnome-tweak-tool
-sudo eopkg install -y gparted htop inkscape meld nautilus-actions nautilus-dropbox neofetch noto-sans-ttf peek plank redshift screenfetch scrot
+sudo eopkg install -y gparted gradio grub-customizer htop inkscape meld nautilus-actions nautilus-dropbox neofetch noto-sans-ttf peek plank redshift screenfetch scrot
 sudo eopkg install -y simplescreenrecorder source-code-pro telegram variety vivaldi-snapshot vivaldi-stable vlc vscode xkill 
 
 #sudo eopkg install -y darktable geany hexchat pidgin tilda
