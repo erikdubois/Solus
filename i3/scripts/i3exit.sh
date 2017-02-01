dec1f456a5915e03184579912a956cc6a9dac809
Erik Dubois <erik.dubois@gmail.com>
2017-02-01 21:27:36 +0100

    Commit comment :  on Wed Feb  1 21:27:35 CET 2017

--- i3/scripts/i3exit.sh
+++ i3/scripts/i3exit.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 lock() {
     #i3lock -c 222222
-    i3lock -i ~/.config/i3/lockscreen.png -t
+    i3lock -i ~/.config/i3/scripts/lockscreen/lockscreen.png -t
 }
 
 case "$1" in
@@ -12,11 +12,9 @@ case "$1" in
         i3-msg exit
         ;;
     suspend)
-        #lock && systemctl supend
-        systemctl supend
+        systemctl suspend
         ;;
     hibernate)
-        #lock && systemctl hibernate
         systemctl hibernate
         ;;
     reboot)
